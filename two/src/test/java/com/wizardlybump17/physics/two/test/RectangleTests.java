91e49e6702c2a488a5203f7bdcb129aeb1773323
WizardlyBump17 <davicraft130@gmail.com>
2024-09-23 17:48:43 -0300

    properly tested

--- two/src/test/java/com/wizardlybump17/physics/two/test/RectangleTests.java
+++ two/src/test/java/com/wizardlybump17/physics/two/test/RectangleTests.java
@@ -20,4 +20,18 @@ class RectangleTests {
         Assertions.assertFalse(new Rectangle(new Vector2D(1, 1), new Vector2D(2, 2)).intersects(new Rectangle(new Vector2D(-1, -1), Vector2D.ZERO)));
         Assertions.assertFalse(new Rectangle(new Vector2D(5, 5), new Vector2D(10, 10)).intersects(new Rectangle(Vector2D.ZERO, new Vector2D(1, -10))));
     }
+
+    @Test
+    void testHasPoints() {
+        Assertions.assertTrue(new Rectangle(Vector2D.ZERO, new Vector2D(1, 1)).hasPoint(Vector2D.ZERO));
+        Assertions.assertTrue(new Rectangle(new Vector2D(1, 1), new Vector2D(10, 10)).hasPoint(new Vector2D(2, 4)));
+        Assertions.assertTrue(new Rectangle(new Vector2D(-6, -9), new Vector2D(5, 9)).hasPoint(Vector2D.ZERO));
+    }
+
+    @Test
+    void testDoesNotHavePoints() {
+        Assertions.assertFalse(new Rectangle(Vector2D.ZERO, new Vector2D(1, 1)).hasPoint(new Vector2D(-1, -1)));
+        Assertions.assertFalse(new Rectangle(new Vector2D(1, 1), new Vector2D(10, 10)).hasPoint(new Vector2D(11, 11)));
+        Assertions.assertFalse(new Rectangle(new Vector2D(-6, -9), new Vector2D(5, 9)).hasPoint(new Vector2D(5.1, 9)));
+    }
 }
