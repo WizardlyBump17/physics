93dd97f2124b6239dd32bc17267dde702164e0d8
WizardlyBump17 <davicraft130@gmail.com>
2024-09-24 18:40:27 -0300

    added some tests for the Rectangle#intersect(Shape) method

--- two/build.gradle.kts
+++ two/build.gradle.kts
@@ -4,5 +4,7 @@ val jetbrainsAnnotations = "24.1.0"
 dependencies {
     compileOnly("org.projectlombok:lombok:${lombok}")
     annotationProcessor("org.projectlombok:lombok:${lombok}")
+    testCompileOnly("org.projectlombok:lombok:${lombok}")
+    testAnnotationProcessor("org.projectlombok:lombok:${lombok}")
     implementation("org.jetbrains:annotations:${jetbrainsAnnotations}")
 }
--- two/src/test/java/com/wizardlybump17/physics/two/test/RectangleTests.java
+++ two/src/test/java/com/wizardlybump17/physics/two/test/RectangleTests.java
@@ -1,7 +1,9 @@
 package com.wizardlybump17.physics.two.test;
 
+import com.wizardlybump17.physics.two.intersection.rectangle.RectangleToRectangleIntersection;
 import com.wizardlybump17.physics.two.position.Vector2D;
 import com.wizardlybump17.physics.two.shape.Rectangle;
+import lombok.NonNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -34,5 +36,45 @@ class RectangleTests {
         Assertions.assertFalse(new Rectangle(Vector2D.ZERO, new Vector2D(1, 1)).hasPoint(new Vector2D(-1, -1)));
         Assertions.assertFalse(new Rectangle(new Vector2D(1, 1), new Vector2D(10, 10)).hasPoint(new Vector2D(11, 11)));
         Assertions.assertFalse(new Rectangle(new Vector2D(-6, -9), new Vector2D(5, 9)).hasPoint(new Vector2D(5.1, 9)));
+    }
+
+    @Test
+    void testIntersections() {
+        Assertions.assertEquals(
+                new RectangleToRectangleIntersection(
+                        create(Vector2D.ZERO, new Vector2D(10, 10)),
+                        create(new Vector2D(5, 5), new Vector2D(15, 15)),
+                        create(new Vector2D(5, 5), new Vector2D(10, 10))
+                ),
+                create(Vector2D.ZERO, new Vector2D(10, 10)).intersect(create(new Vector2D(5, 5), new Vector2D(15, 15)))
+        );
+        Assertions.assertEquals(
+                new RectangleToRectangleIntersection(
+                        create(Vector2D.ZERO, new Vector2D(10, 10)),
+                        create(new Vector2D(5, 5), new Vector2D(7, 7)),
+                        create(new Vector2D(5, 5), new Vector2D(7, 7))
+                ),
+                create(Vector2D.ZERO, new Vector2D(10, 10)).intersect(create(new Vector2D(5, 5), new Vector2D(7, 7)))
+        );
+        Assertions.assertEquals(
+                new RectangleToRectangleIntersection(
+                        create(new Vector2D(-10, 10), Vector2D.ZERO),
+                        create(new Vector2D(5, 5), new Vector2D(-15, 15)),
+                        create(new Vector2D(0, 5), new Vector2D(-10, 10))
+                ),
+                create(Vector2D.ZERO, new Vector2D(-10, 10)).intersect(create(new Vector2D(5, 5), new Vector2D(-15, 15)))
+        );
+        Assertions.assertEquals(
+                new RectangleToRectangleIntersection(
+                        create(new Vector2D(-10, 7), new Vector2D(0, 10)),
+                        create(new Vector2D(5, 5), new Vector2D(-15, 15)),
+                        create(new Vector2D(-10, 7), new Vector2D(0, 10))
+                ),
+                create(new Vector2D(-10, 7), new Vector2D(0, 10)).intersect(create(new Vector2D(5, 5), new Vector2D(-15, 15)))
+        );
+    }
+
+    static @NonNull Rectangle create(@NonNull Vector2D min, @NonNull Vector2D max) {
+        return new Rectangle(min, max);
     }
 }
