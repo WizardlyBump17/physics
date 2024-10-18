8cbd8b326183ed68626aaf40faea2955c017c0fd
WizardlyBump17 <davicraft130@gmail.com>
2024-10-17 22:03:12 -0300

    improved the safe position

--- two/src/main/java/com/wizardlybump17/physics/two/intersection/rectangle/RectangleToCircleIntersection.java
+++ two/src/main/java/com/wizardlybump17/physics/two/intersection/rectangle/RectangleToCircleIntersection.java
@@ -20,6 +20,34 @@ public class RectangleToCircleIntersection implements Intersection {
 
     @Override
     public @NonNull Vector2D getSafePosition() {
-        return circle.getPosition();
+        Vector2D circlePosition = circle.getPosition();
+        Vector2D rectanglePosition = rectangle.getPosition();
+        Vector2D rectangleMin = rectangle.getMin();
+        Vector2D rectangleMax = rectangle.getMax();
+
+        double clampedX = Math.clamp(circlePosition.x(), rectangleMin.x(), rectangleMax.x());
+        double clampedY = Math.clamp(circlePosition.y(), rectangleMin.y(), rectangleMax.y());
+        double realClosestX;
+        double realClosestY;
+        double xRatio = 0;
+        double yRatio;
+
+        if (rectangle.hasPoint(circlePosition)) {
+            xRatio = circlePosition.x() / rectanglePosition.x();
+            yRatio = circlePosition.y() / rectanglePosition.y();
+
+            realClosestX = xRatio >= yRatio ? (circlePosition.x() > rectanglePosition.x() ? rectangleMax.x() : rectangleMin.x()) : clampedX;
+            realClosestY = yRatio > xRatio ? (circlePosition.y() > rectanglePosition.y() ? rectangleMax.y() : rectangleMin.y()) : clampedY;
+        } else {
+            realClosestX = Math.clamp(circlePosition.x(), rectangleMin.x(), rectangleMax.x());
+            realClosestY = Math.clamp(circlePosition.y(), rectangleMin.y(), rectangleMax.y());
+        }
+
+        Vector2D closestPoint = new Vector2D(realClosestX, realClosestY);
+        Vector2D subtract = circlePosition.subtract(closestPoint);
+        Vector2D direction = subtract.normalize();
+
+        double multiplier = circle.getRadius() + 0.00000000001;
+        return closestPoint.add(direction.multiply(xRatio != 0 ? -multiplier : multiplier));
     }
 }
