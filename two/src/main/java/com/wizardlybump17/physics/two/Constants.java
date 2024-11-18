9ea19d0d13fda2906a4460eb2d40d6fbf1b619f5
WizardlyBump17 <davicraft130@gmail.com>
2024-11-18 19:31:16 -0300

    increase the number of ticks per second

--- two/src/main/java/com/wizardlybump17/physics/two/Constants.java
+++ two/src/main/java/com/wizardlybump17/physics/two/Constants.java
@@ -2,7 +2,7 @@ package com.wizardlybump17.physics.two;
 
 public final class Constants {
 
-    public static final int TICKS_PER_SECOND = 20;
+    public static final int TICKS_PER_SECOND = 128;
 
     private Constants() {
     }
