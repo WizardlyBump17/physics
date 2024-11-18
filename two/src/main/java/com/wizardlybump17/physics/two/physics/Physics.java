83f0084c855882bb85fd070e46f5c67f00196cfb
WizardlyBump17 <davicraft130@gmail.com>
2024-11-17 21:03:58 -0300

    added the Physics#tick(PhysicsObject, double) method

--- two/src/main/java/com/wizardlybump17/physics/two/physics/Physics.java
+++ two/src/main/java/com/wizardlybump17/physics/two/physics/Physics.java
@@ -1,4 +1,12 @@
 package com.wizardlybump17.physics.two.physics;
 
+import com.wizardlybump17.physics.two.physics.object.PhysicsObject;
+import org.jetbrains.annotations.NotNull;
+
 public class Physics {
+
+    public void tick(@NotNull PhysicsObject object, double deltaTime) {
+        object.setVelocity(object.getVelocity().add(object.getAcceleration()));
+        object.teleport(object.getPosition().add(object.getVelocity()));
+    }
 }
