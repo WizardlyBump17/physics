2f218fc1a66e766be2b13dc08f36b20417d857ff
WizardlyBump17 <davicraft130@gmail.com>
2024-11-17 21:32:46 -0300

    using the Rectangle#getClosestPoint(Vector2D) method for now

--- two/src/main/java/com/wizardlybump17/physics/two/shape/Rectangle.java
+++ two/src/main/java/com/wizardlybump17/physics/two/shape/Rectangle.java
@@ -141,13 +141,7 @@ public class Rectangle extends Shape {
 
     @Override
     public @NotNull Vector2D getClosestPoint(@NotNull Shape shape) {
-        return switch (shape) {
-            case Circle circle -> new Vector2D(
-                    Math.clamp(circle.getPosition().x(), min.x(), max.x()),
-                    Math.clamp(circle.getPosition().y(), min.y(), max.y())
-            );
-            default -> Vector2D.ZERO;
-        };
+        return getClosestPoint(shape.getPosition());
     }
 
     public @NotNull Line getUpperLine() {
