979075cc92f22a6d3bfda013dbcf050126ef74b3
WizardlyBump17 <davicraft130@gmail.com>
2024-11-22 19:11:46 -0300

    added the BaseObjectContainer#collisions(BaseObject) method

--- two/src/main/java/com/wizardlybump17/physics/two/container/BaseObjectContainer.java
+++ two/src/main/java/com/wizardlybump17/physics/two/container/BaseObjectContainer.java
@@ -1,6 +1,8 @@
 package com.wizardlybump17.physics.two.container;
 
+import com.wizardlybump17.physics.two.intersection.Intersection;
 import com.wizardlybump17.physics.two.object.BaseObject;
+import com.wizardlybump17.physics.two.shape.Shape;
 import com.wizardlybump17.physics.two.tick.Ticker;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -47,4 +49,26 @@ public abstract class BaseObjectContainer implements Ticker {
     protected abstract @NotNull Collection<BaseObject> getObjectsInternal();
 
     public abstract void clear();
+
+    /**
+     * <p>
+     * Handles the collision for the given object.
+     * This method calls the {@link BaseObject#onCollide(BaseObject, Intersection)} of that object,
+     * passing the other object (in the loop) and their {@link Intersection}s.
+     * </p>
+     *
+     * @param object the object to handle
+     */
+    public void collisions(@NotNull BaseObject object) {
+        for (BaseObject other : getObjectsInternal()) {
+            Shape objectShape = object.getShape();
+            Shape otherShape = other.getShape();
+
+            Intersection intersection = objectShape.intersect(otherShape);
+            if (!intersection.intersects())
+                continue;
+
+            object.onCollide(other, intersection);
+        }
+    }
 }
