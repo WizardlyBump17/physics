8756d743705dd12dd47d84744fa67e9d140f9b2b
WizardlyBump17 <davicraft130@gmail.com>
2024-11-21 23:49:51 -0300

    made the PhysicsObject extend the BaseObject

--- two/src/main/java/com/wizardlybump17/physics/two/physics/object/PhysicsObject.java
+++ two/src/main/java/com/wizardlybump17/physics/two/physics/object/PhysicsObject.java
@@ -1,48 +1,24 @@
 package com.wizardlybump17.physics.two.physics.object;
 
+import com.wizardlybump17.physics.two.object.BaseObject;
 import com.wizardlybump17.physics.two.physics.Physics;
-import com.wizardlybump17.physics.two.position.Vector2D;
 import com.wizardlybump17.physics.two.shape.Shape;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
-public class PhysicsObject {
+public class PhysicsObject extends BaseObject {
 
-    private final int id;
-    private @NotNull Shape shape;
     private @NotNull Physics physics;
 
     public PhysicsObject(int id, @NotNull Shape shape, @NotNull Physics physics) {
-        this.id = id;
-        this.shape = shape;
+        super(id, shape);
         this.physics = physics;
     }
 
     public PhysicsObject(int id, @NotNull Shape shape) {
-        this.id = id;
-        this.shape = shape;
+        super(id, shape);
         this.physics = new Physics(this);
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public @NotNull Vector2D getPosition() {
-        return shape.getPosition();
-    }
-
-    public void teleport(@NotNull Vector2D position) {
-        shape = shape.at(position);
-    }
-
-    public @NotNull Shape getShape() {
-        return shape;
-    }
-
-    public void setShape(@NotNull Shape shape) {
-        this.shape = shape;
     }
 
     public @NotNull Physics getPhysics() {
@@ -53,7 +29,9 @@ public class PhysicsObject {
         this.physics = physics;
     }
 
+    @Override
     public void tick(double deltaTime) {
+        super.tick(deltaTime);
         physics.tick(deltaTime);
     }
 
@@ -61,21 +39,21 @@ public class PhysicsObject {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass())
             return false;
+        if (!super.equals(o))
+            return false;
         PhysicsObject that = (PhysicsObject) o;
-        return id == that.id && Objects.equals(shape, that.shape) && Objects.equals(physics, that.physics);
+        return Objects.equals(physics, that.physics);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, shape, physics);
+        return Objects.hash(super.hashCode(), physics);
     }
 
     @Override
     public String toString() {
         return "PhysicsObject{" +
-                "id=" + id +
-                ", shape=" + shape +
-                ", physics=" + physics +
-                '}';
+                "physics=" + physics +
+                "} " + super.toString();
     }
 }
