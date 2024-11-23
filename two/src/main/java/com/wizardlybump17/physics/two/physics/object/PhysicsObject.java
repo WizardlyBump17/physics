8f6d178b0b7f95aef21feda7ab97e1cc2adc4f6a
WizardlyBump17 <davicraft130@gmail.com>
2024-11-22 21:38:07 -0300

    added back the collision system

--- two/src/main/java/com/wizardlybump17/physics/two/physics/object/PhysicsObject.java
+++ two/src/main/java/com/wizardlybump17/physics/two/physics/object/PhysicsObject.java
@@ -1,16 +1,20 @@
 package com.wizardlybump17.physics.two.physics.object;
 
 import com.wizardlybump17.physics.two.container.BaseObjectContainer;
+import com.wizardlybump17.physics.two.intersection.Intersection;
 import com.wizardlybump17.physics.two.object.BaseObject;
 import com.wizardlybump17.physics.two.physics.Physics;
 import com.wizardlybump17.physics.two.shape.Shape;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class PhysicsObject extends BaseObject {
 
     private @NotNull Physics physics;
+    protected final @NotNull Map<Integer, Collision> collisions = new HashMap<>();
 
     public PhysicsObject(@NotNull BaseObjectContainer container, int id, @NotNull Shape shape, @NotNull Physics physics) {
         super(container, id, shape);
@@ -33,6 +37,7 @@ public class PhysicsObject extends BaseObject {
     @Override
     public void tick(double deltaTime) {
         super.tick(deltaTime);
+        handleCollisions();
         physics.tick(deltaTime);
     }
 
@@ -56,5 +61,39 @@ public class PhysicsObject extends BaseObject {
         return "PhysicsObject{" +
                 "physics=" + physics +
                 "} " + super.toString();
+    }
+
+    protected void handleCollisions() {
+    }
+
+    /**
+     * <p>
+     * This method is called when this object collides with another one.
+     * </p>
+     *
+     * @param other        the {@link BaseObject} is colliding with this one
+     * @param intersection the {@link Intersection}
+     */
+    public void onCollide(@NotNull BaseObject other, @NotNull Intersection intersection) {
+        collisions.put(other.getId(), new Collision(other, intersection));
+    }
+
+    public @NotNull Map<Integer, Collision> getCollisions() {
+        return Map.copyOf(collisions);
+    }
+
+    public boolean isCollidingWith(@NotNull BaseObject other) {
+        return collisions.containsKey(other.getId());
+    }
+
+    public void onCollisionStop(@NotNull BaseObject other) {
+        collisions.remove(other.getId());
+    }
+
+    public boolean hasCollisions() {
+        return !collisions.isEmpty();
+    }
+
+    public record Collision(@NotNull BaseObject object, @NotNull Intersection intersection) {
     }
 }
