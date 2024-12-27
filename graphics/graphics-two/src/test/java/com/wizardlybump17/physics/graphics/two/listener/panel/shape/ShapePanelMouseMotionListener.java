e3da0ca2dd4ce4c1ca5b42a0abf7bfe4d23e80c2
WizardlyBump17 <davicraft130@gmail.com>
2024-12-26 21:44:57 -0300

    applying the velocity instead

--- graphics/graphics-two/src/test/java/com/wizardlybump17/physics/graphics/two/listener/panel/shape/ShapePanelMouseMotionListener.java
+++ graphics/graphics-two/src/test/java/com/wizardlybump17/physics/graphics/two/listener/panel/shape/ShapePanelMouseMotionListener.java
@@ -2,7 +2,11 @@ package com.wizardlybump17.physics.graphics.two.listener.panel.shape;
 
 import com.wizardlybump17.physics.graphics.two.panel.object.ObjectsPanel;
 import com.wizardlybump17.physics.graphics.two.panel.object.PanelObject;
+import com.wizardlybump17.physics.two.Constants;
 import com.wizardlybump17.physics.two.Engine;
+import com.wizardlybump17.physics.two.object.BaseObject;
+import com.wizardlybump17.physics.two.physics.Physics;
+import com.wizardlybump17.physics.two.physics.object.PhysicsObject;
 import com.wizardlybump17.physics.two.position.Vector2D;
 import org.jetbrains.annotations.NotNull;
 
@@ -20,8 +24,20 @@ public class ShapePanelMouseMotionListener extends MouseAdapter {
     @Override
     public void mouseDragged(@NotNull MouseEvent event) {
         PanelObject panelObject = panel.getSelectedShape();
-        if (panelObject != null)
-            Engine.getScheduler().schedule(() -> panelObject.getObject().teleport(new Vector2D(event.getX(), event.getY())));
+        if (panelObject == null)
+            return;
+
+        Engine.getScheduler().schedule(() -> {
+            BaseObject object = panelObject.getObject();
+            Vector2D position = object.getPosition();
+            Vector2D target = new Vector2D(event.getX(), event.getY());
+            if (object instanceof PhysicsObject physicsObject) {
+                Physics physics = physicsObject.getPhysics();
+                physics.setAcceleration(Vector2D.ZERO);
+                physics.setVelocity(target.subtract(position).multiply(Constants.TICKS_PER_SECOND / 2.0));
+            } else
+                object.teleport(target);
+        });
     }
 
     public @NotNull ObjectsPanel getPanel() {
