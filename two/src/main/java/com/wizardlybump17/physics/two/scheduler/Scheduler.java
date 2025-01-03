f6a075d66248b1cc522397ea8db0ebd229f53fcb
WizardlyBump17 <davicraft130@gmail.com>
2025-01-02 23:36:45 -0300

    reworked the scheduler

--- two/src/main/java/com/wizardlybump17/physics/two/scheduler/Scheduler.java
+++ two/src/main/java/com/wizardlybump17/physics/two/scheduler/Scheduler.java
@@ -1,106 +1,84 @@
 package com.wizardlybump17.physics.two.scheduler;
 
-import com.wizardlybump17.physics.two.scheduler.task.AbstractTask;
 import com.wizardlybump17.physics.two.scheduler.task.RunningTask;
-import com.wizardlybump17.physics.two.scheduler.task.Task;
+import com.wizardlybump17.physics.two.scheduler.task.factory.RunningTaskFactory;
 import com.wizardlybump17.physics.two.tick.Ticker;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Scheduler implements Ticker {
 
-    private long tickCounter;
-    private long taskCounter;
-    private @NotNull Tick currentTick = new Tick(tickCounter++);
-    private @NotNull Tick nextTick = new Tick(tickCounter++);
-    private final @NotNull Map<Long, Tick> scheduledTicks = new TreeMap<>();
-    private final @NotNull Map<Long, AbstractTask> tasks = new TreeMap<>();
+    private long currentTick;
+    private long tickStart;
+    private long tickEnd;
+    private final @NotNull Map<Long, RunningTask> tasks = new HashMap<>();
+    private final @NotNull RunningTaskFactory taskFactory;
 
-    public Scheduler() {
-        scheduledTicks.put(currentTick.getTick(), currentTick);
-        scheduledTicks.put(nextTick.getTick(), nextTick);
+    public Scheduler(@NotNull RunningTaskFactory taskFactory) {
+        this.taskFactory = taskFactory;
     }
 
-    public @NotNull RunningTask schedule(@NotNull AbstractTask task) {
-        tasks.put(taskCounter++, task);
-        return taskCounter - 1;
+    protected @NotNull RunningTask addTask(@NotNull RunningTask task) {
+        tasks.put(task.getId(), task);
+        return task;
     }
 
-    public @NotNull RunningTask schedule(@NotNull AbstractTask task, long delay) {
-        long id = schedule(task);
-        schedule(task::run, delay);
-        return id;
+    public @NotNull RunningTask schedule(@NotNull Runnable task) {
+        return addTask(taskFactory.create(getTaskCount(), task, currentTick + 1));
     }
 
-    public @NotNull RunningTask schedule(@NotNull AbstractTask task, long delay, long period) {
-        long id = schedule(task);
-        schedule(task::run, delay, period);
-        return id;
-    }
-
-    public @NotNull RunningTask schedule(@NotNull Task task) {
-        nextTick.addTask(task);
-        return taskCounter++;
-    }
-
-    public @NotNull RunningTask schedule(@NotNull Task task, long delay) {
-        if (delay < 2)
-            nextTick.addTask(task);
-        else
-            scheduledTicks.computeIfAbsent(tickCounter + delay, $ -> new Tick(tickCounter + delay)).addTask(task);
-        return taskCounter++;
+    public @NotNull RunningTask schedule(@NotNull Runnable task, long delay) {
+        return addTask(taskFactory.create(getTaskCount(), task, delay, currentTick + 1));
     }
 
     public @NotNull RunningTask schedule(@NotNull Runnable task, long delay, long period) {
-        if (period < 1)
-            return schedule(task::run, delay);
-
-        long targetTick = tickCounter + delay;
-        schedule(new Task() {
-            @Override
-            public void run() {
-                task.run();
-            }
-
-            @Override
-            public boolean canRun() {
-                return (tickCounter - targetTick) % period == 0;
-            }
-
-            @Override
-            public boolean isPersistent() {
-                return true;
-            }
-        }, delay);
-        return taskCounter++;
+        return addTask(taskFactory.create(getTaskCount(), task, delay, period, currentTick + 1));
     }
 
     @Override
     public void run() {
-        Tick currentTick = this.currentTick;
-        currentTick.start();
+        tickStart = System.currentTimeMillis();
 
-        currentTick.tick();
+        Iterator<RunningTask> taskIterator = tasks.values().iterator();
+        while (taskIterator.hasNext()) {
+            RunningTask task = taskIterator.next();
+            if (!task.isRunning()) {
+                taskIterator.remove();
+                continue;
+            }
 
-        scheduledTicks.remove(currentTick.getTick());
+            if (task.isTimeToRun(currentTick))
+                task.run();
 
-        this.currentTick = this.nextTick;
-        this.currentTick.addTasks(currentTick.getTasksToReschedule());
+            if (!task.isRepeatable() || !task.isRunning())
+                taskIterator.remove();
+        }
 
-        this.nextTick = scheduledTicks.computeIfAbsent(++tickCounter, $ -> new Tick(tickCounter));
+        tickEnd = System.currentTimeMillis();
 
-        currentTick.end();
-//
-//        long elapsedTime = currentTick.getElapsedTime();
-//        if (elapsedTime > Constants.MILLIS_PER_TICK)
-//            System.out.println(Constants.MILLIS_PER_TICK / elapsedTime * Constants.TICKS_PER_SECOND);
-//        else
-//            System.out.println(Constants.TICKS_PER_SECOND);
+        currentTick++;
     }
 
     public long getCurrentTick() {
-        return currentTick.getTick();
+        return currentTick;
+    }
+
+    public @NotNull RunningTaskFactory getTaskFactory() {
+        return taskFactory;
+    }
+
+    public long getTaskCount() {
+        return tasks.size();
+    }
+
+    public long getTickStart() {
+        return tickStart;
+    }
+
+    public long getTickEnd() {
+        return tickEnd;
     }
 }
