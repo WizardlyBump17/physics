b39c668bbe68f912c31b732a0539f749d56263c0
WizardlyBump17 <davicraft130@gmail.com>
2025-01-02 23:04:46 -0300

    renamed the class and made it import RunningTask

--- two/src/main/java/com/wizardlybump17/physics/two/scheduler/task/BasicTask.java
+++ two/src/main/java/com/wizardlybump17/physics/two/scheduler/task/BasicTask.java
@@ -4,43 +4,49 @@ import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 @ApiStatus.Internal
-public class InternalTask {
+public class BasicTask implements RunningTask {
 
     private final long id;
     private boolean running = true;
-    private final long startedAtTick;
+    private final long startedAt;
     private final long delay;
     private final long period;
     private final @NotNull Runnable runnable;
 
-    public InternalTask(long id, long startedAtTick, long delay, long period, @NotNull Runnable runnable) {
+    public BasicTask(long id, long startedAt, long delay, long period, @NotNull Runnable runnable) {
         this.id = id;
-        this.startedAtTick = startedAtTick;
+        this.startedAt = startedAt;
         this.delay = delay;
         this.period = period;
         this.runnable = runnable;
     }
 
+    @Override
     public long getId() {
         return id;
     }
 
+    @Override
     public boolean isRunning() {
         return running;
     }
 
+    @Override
     public void setRunning(boolean running) {
         this.running = running;
     }
 
-    public long getStartedAtTick() {
-        return startedAtTick;
+    @Override
+    public long getStartedAt() {
+        return startedAt;
     }
 
+    @Override
     public long getDelay() {
         return delay;
     }
 
+    @Override
     public long getPeriod() {
         return period;
     }
@@ -49,7 +55,13 @@ public class InternalTask {
         return runnable;
     }
 
+    @Override
     public boolean isTimeToRun(long currentTick) {
-        return currentTick % startedAtTick + delay == 0;
+        return currentTick % startedAt + delay == 0;
+    }
+
+    @Override
+    public void run() {
+        runnable.run();
     }
 }
