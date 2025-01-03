2f761537c4b4142bae47c670776b4766357a3a61
WizardlyBump17 <davicraft130@gmail.com>
2025-01-02 23:15:45 -0300

    added some convenience methods to create tasks

--- two/src/main/java/com/wizardlybump17/physics/two/scheduler/task/factory/RunningTaskFactory.java
+++ two/src/main/java/com/wizardlybump17/physics/two/scheduler/task/factory/RunningTaskFactory.java
@@ -6,4 +6,12 @@ import org.jetbrains.annotations.NotNull;
 public abstract class RunningTaskFactory {
 
     public abstract @NotNull RunningTask create(long id, @NotNull Runnable task, long delay, long period, long startedAt);
+
+    public @NotNull RunningTask create(long id, @NotNull Runnable task, long startedAt) {
+        return create(id, task, 0, 0, startedAt);
+    }
+
+    public @NotNull RunningTask create(long id, @NotNull Runnable task, long delay, long startedAt) {
+        return create(id, task, delay, 0, startedAt);
+    }
 }
