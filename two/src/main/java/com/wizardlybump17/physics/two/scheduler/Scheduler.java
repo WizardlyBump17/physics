55fdab3d5dfb4643193f4a6d6150272059436752
WizardlyBump17 <davicraft130@gmail.com>
2025-01-05 22:03:33 -0300

    improved the Scheduler

--- two/src/main/java/com/wizardlybump17/physics/two/scheduler/Scheduler.java
+++ two/src/main/java/com/wizardlybump17/physics/two/scheduler/Scheduler.java
@@ -15,27 +15,29 @@ public class Scheduler implements Ticker {
     private long tickStart;
     private long tickEnd;
     private final @NotNull Map<Long, RunningTask> tasks = new HashMap<>();
+    private final @NotNull Map<Long, RunningTask> pendingTasks = new HashMap<>();
     private final @NotNull RunningTaskFactory taskFactory;
+    private long taskCounter;
 
     public Scheduler(@NotNull RunningTaskFactory taskFactory) {
         this.taskFactory = taskFactory;
     }
 
     protected @NotNull RunningTask addTask(@NotNull RunningTask task) {
-        tasks.put(task.getId(), task);
+        pendingTasks.put(task.getId(), task);
         return task;
     }
 
     public @NotNull RunningTask schedule(@NotNull Runnable task) {
-        return addTask(taskFactory.create(getTaskCount(), task, currentTick + 1));
+        return addTask(taskFactory.create(nextTaskId(), task, currentTick + 1));
     }
 
     public @NotNull RunningTask schedule(@NotNull Runnable task, long delay) {
-        return addTask(taskFactory.create(getTaskCount(), task, delay, currentTick + 1));
+        return addTask(taskFactory.create(nextTaskId(), task, delay, currentTick + 1));
     }
 
     public @NotNull RunningTask schedule(@NotNull Runnable task, long delay, long period) {
-        return addTask(taskFactory.create(getTaskCount(), task, delay, period, currentTick + 1));
+        return addTask(taskFactory.create(nextTaskId(), task, delay, period, currentTick + 1));
     }
 
     public void cancelTask(long id) {
@@ -47,6 +49,8 @@ public class Scheduler implements Ticker {
     public void run() {
         tickStart = System.currentTimeMillis();
 
+        tasks.putAll(pendingTasks);
+        pendingTasks.clear();
         Iterator<RunningTask> taskIterator = tasks.values().iterator();
         while (taskIterator.hasNext()) {
             RunningTask task = taskIterator.next();
@@ -61,6 +65,8 @@ public class Scheduler implements Ticker {
             if (!task.isRepeatable() || !task.isRunning())
                 taskIterator.remove();
         }
+        pendingTasks.putAll(tasks);
+        tasks.clear();
 
         tickEnd = System.currentTimeMillis();
 
@@ -75,8 +81,8 @@ public class Scheduler implements Ticker {
         return taskFactory;
     }
 
-    public long getTaskCount() {
-        return tasks.size();
+    protected long nextTaskId() {
+        return taskCounter++;
     }
 
     public long getTickStart() {
