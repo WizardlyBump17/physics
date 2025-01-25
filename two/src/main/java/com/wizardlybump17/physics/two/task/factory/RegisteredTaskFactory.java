5c3d0cb2f260dbf78fed6f8e503fa1d00b8d7eae
WizardlyBump17 <davicraft130@gmail.com>
2025-01-25 19:06:28 -0300

    marked as internal

--- two/src/main/java/com/wizardlybump17/physics/two/task/factory/RegisteredTaskFactory.java
+++ two/src/main/java/com/wizardlybump17/physics/two/task/factory/RegisteredTaskFactory.java
@@ -1,8 +1,10 @@
 package com.wizardlybump17.physics.two.task.factory;
 
 import com.wizardlybump17.physics.two.task.RegisteredTask;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
+@ApiStatus.Internal
 public class RegisteredTaskFactory {
 
     public @NotNull RegisteredTask create(int id, @NotNull Runnable task, long delay, long period, long startedAt) {
