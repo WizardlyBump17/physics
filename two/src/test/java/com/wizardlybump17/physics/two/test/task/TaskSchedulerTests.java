a68fb7630a7f30b6373dc07bf824f2aa5d6a2620
WizardlyBump17 <davicraft130@gmail.com>
2025-01-31 17:42:07 -0300

    added some tests for the Task class

--- two/src/test/java/com/wizardlybump17/physics/two/test/task/TaskSchedulerTests.java
+++ two/src/test/java/com/wizardlybump17/physics/two/test/task/TaskSchedulerTests.java
@@ -1,6 +1,8 @@
 package com.wizardlybump17.physics.two.test.task;
 
+import com.wizardlybump17.physics.two.task.Task;
 import com.wizardlybump17.physics.two.task.factory.RegisteredTaskFactory;
+import com.wizardlybump17.physics.two.task.registered.RegisteredTask;
 import com.wizardlybump17.physics.two.task.scheduler.TaskScheduler;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.*;
@@ -83,5 +85,123 @@ class TaskSchedulerTests {
             scheduler.tick();
 
         Assertions.assertEquals(tasks, executedTasks.get());
+    }
+
+    @Test
+    @Order(3)
+    void testScheduleCancelAndIsScheduled() {
+        int tasks = 10;
+        AtomicInteger executedTasks = new AtomicInteger();
+
+        for (int i = 0; i < tasks; i++) {
+            RegisteredTask task = scheduler.schedule(executedTasks::incrementAndGet);
+            Assertions.assertTrue(scheduler.isScheduled(task.getId()));
+            task.cancel();
+            Assertions.assertFalse(scheduler.isScheduled(task.getId()));
+        }
+
+        scheduler.tick();
+        for (int i = 0; i < tasks; i++)
+            scheduler.tick();
+
+        Assertions.assertEquals(0, executedTasks.get());
+    }
+
+    @Test
+    @Order(4)
+    void testTaskSchedule() {
+        int tasks = 10;
+        AtomicInteger executedTasks = new AtomicInteger();
+
+        for (int i = 0; i < tasks; i++) {
+            new Task() {
+                @Override
+                public void run() {
+                    executedTasks.incrementAndGet();
+                }
+            }.schedule(scheduler);
+        }
+
+        scheduler.tick();
+        for (int i = 0; i < tasks; i++)
+            scheduler.tick();
+
+        Assertions.assertEquals(tasks, executedTasks.get());
+    }
+
+    @Test
+    @Order(5)
+    void testTaskScheduleWithDelay() {
+        int tasks = 10;
+        long delay = 20;
+        AtomicInteger executedTasks = new AtomicInteger();
+
+        for (int i = 0; i < tasks; i++) {
+            new Task() {
+                @Override
+                public void run() {
+                    executedTasks.incrementAndGet();
+                }
+            }.schedule(scheduler, delay);
+        }
+
+        scheduler.tick();
+        for (int i = 0; i < tasks * delay; i++)
+            scheduler.tick();
+
+        Assertions.assertEquals(tasks, executedTasks.get());
+    }
+
+    @Test
+    @Order(6)
+    void testTaskScheduleWithDelayAndPeriod() {
+        int tasks = 10;
+        long delay = 20;
+        long period = 10;
+        AtomicInteger executedTasks = new AtomicInteger();
+
+        for (int i = 0; i < tasks; i++) {
+            new Task() {
+                int count;
+
+                @Override
+                public void run() {
+                    count++;
+                    if (count >= tasks)
+                        executedTasks.incrementAndGet();
+                }
+            }.schedule(scheduler, delay, period);
+        }
+
+        scheduler.tick();
+        for (int i = 0; i < delay; i++)
+            scheduler.tick();
+        for (int i = 0; i < tasks * period; i++)
+            scheduler.tick();
+
+        Assertions.assertEquals(tasks, executedTasks.get());
+    }
+
+    @Test
+    @Order(7)
+    void testTaskScheduleCancelAndIsScheduled() {
+        int tasks = 10;
+        AtomicInteger executedTasks = new AtomicInteger();
+
+        for (int i = 0; i < tasks; i++) {
+            RegisteredTask registeredTask = new Task() {
+                @Override
+                public void run() {
+                    executedTasks.incrementAndGet();
+                }
+            }.schedule(scheduler);
+            int id = registeredTask.getId();
+
+            Assertions.assertTrue(scheduler.isScheduled(id));
+            registeredTask.cancel();
+            Assertions.assertFalse(scheduler.isScheduled(id));
+        }
+
+        Assertions.assertEquals(0, executedTasks.get());
     }
 }
