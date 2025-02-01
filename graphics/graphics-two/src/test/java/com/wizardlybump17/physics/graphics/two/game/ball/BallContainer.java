d149b251c20c089c02b66e1772cd480e518d096f
WizardlyBump17 <davicraft130@gmail.com>
2025-02-01 15:46:13 -0300

    added the BallContainer#ball field

--- graphics/graphics-two/src/test/java/com/wizardlybump17/physics/graphics/two/game/ball/BallContainer.java
+++ graphics/graphics-two/src/test/java/com/wizardlybump17/physics/graphics/two/game/ball/BallContainer.java
@@ -1,5 +1,6 @@
 package com.wizardlybump17.physics.graphics.two.game.ball;
 
+import com.wizardlybump17.physics.graphics.two.game.ball.ball.Ball;
 import com.wizardlybump17.physics.two.container.BasicBaseObjectContainer;
 import com.wizardlybump17.physics.two.object.BaseObject;
 import com.wizardlybump17.physics.two.position.Vector2D;
@@ -10,13 +11,23 @@ import java.util.UUID;
 
 public class BallContainer extends BasicBaseObjectContainer {
 
+    private @NotNull Ball ball;
+
     public BallContainer(@NotNull UUID id) {
         super(id);
         init();
     }
 
     protected void init() {
-        addObject(new BaseObject(this, 0, new Circle(new Vector2D(250, 250), 50)));
+        addObject(ball = new Ball(this, 0, new Circle(new Vector2D(250, 250), 12.5)));
         addObject(new BaseObject(this, 1, new Circle(new Vector2D(250, 0), 50)));
+    }
+
+    public @NotNull Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(@NotNull Ball ball) {
+        this.ball = ball;
     }
 }
