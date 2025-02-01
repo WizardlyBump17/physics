076f49f447383553bac4a043d3b8905166767547
WizardlyBump17 <davicraft130@gmail.com>
2025-02-01 16:36:11 -0300

    added methods to move the platform

--- graphics/graphics-two/src/test/java/com/wizardlybump17/physics/graphics/two/game/ball/platform/PlatformPhysics.java
+++ graphics/graphics-two/src/test/java/com/wizardlybump17/physics/graphics/two/game/ball/platform/PlatformPhysics.java
@@ -7,6 +7,8 @@ import org.jetbrains.annotations.NotNull;
 
 public class PlatformPhysics extends Physics {
 
+    private @NotNull Direction direction = Direction.NONE;
+
     public PlatformPhysics(@NotNull Platform object, @NotNull Vector2D velocity, @NotNull Vector2D acceleration) {
         super(object, velocity, acceleration);
     }
@@ -25,5 +27,32 @@ public class PlatformPhysics extends Physics {
         if (!(object instanceof Platform))
             throw new IllegalArgumentException("Expected a " + Platform.class.getSimpleName() + " but got " + object.getClass().getSimpleName());
         super.setObject(object);
+    }
+
+    public void moveLeft() {
+        direction = Direction.LEFT;
+        setVelocity(new Vector2D(-100, 0));
+    }
+
+    public void moveRight() {
+        direction = Direction.RIGHT;
+        setVelocity(new Vector2D(100, 0));
+    }
+
+    public void stopMovement() {
+        direction = Direction.NONE;
+        setVelocity(Vector2D.ZERO);
+    }
+
+    public @NotNull Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(@NotNull Direction direction) {
+        this.direction = direction;
+    }
+
+    public enum Direction {
+        LEFT, RIGHT, NONE;
     }
 }
