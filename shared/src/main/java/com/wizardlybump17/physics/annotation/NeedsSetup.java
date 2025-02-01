69c505b2b8e9e528b55991d7b776782b1de085bf
WizardlyBump17 <davicraft130@gmail.com>
2025-02-01 17:08:50 -0300

    referencing the SetupMethod annotation on the Javadoc

--- shared/src/main/java/com/wizardlybump17/physics/annotation/NeedsSetup.java
+++ shared/src/main/java/com/wizardlybump17/physics/annotation/NeedsSetup.java
@@ -9,6 +9,11 @@ import java.lang.annotation.Target;
  * the constructor. This may happen when there is a crucial field that can not be null, but it is not assigned by the constructor
  * and needs to be set with a setter method right after the constructor call.
  * </p>
+ * <p>
+ * Generally if this annotation is present, there is a method annotated with the {@link SetupMethod} annotation.
+ * </p>
+ *
+ * @see SetupMethod
  */
 @Target(ElementType.CONSTRUCTOR)
 public @interface NeedsSetup {
