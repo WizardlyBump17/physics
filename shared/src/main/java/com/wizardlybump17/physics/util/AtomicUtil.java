054c0c295b30288e80c374cd18e8b9c27ce44b4d
WizardlyBump17 <davicraft130@gmail.com>
2025-01-31 22:00:40 -0300

    returning the old value held

--- shared/src/main/java/com/wizardlybump17/physics/util/AtomicUtil.java
+++ shared/src/main/java/com/wizardlybump17/physics/util/AtomicUtil.java
@@ -7,9 +7,27 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public final class AtomicUtil {
 
-    public static <T> void set(@NotNull AtomicReference<T> reference, @Nullable T newValue) {
+    /**
+     * <p>
+     * Sets the value of the {@link AtomicReference} to be the given new value.
+     * It will keep trying to set the value while the {@link AtomicReference#get()} differs from the actual value held by the {@link AtomicReference}.
+     * </p>
+     * <p>
+     * This method will return the previous value held by the {@link AtomicReference}.
+     * </p>
+     *
+     * @param reference the {@link AtomicReference} to change
+     * @param newValue  the new value to be set
+     * @param <T>       the type of the value
+     * @return the old value held by the {@link AtomicReference}
+     */
+    public static <T> T set(@NotNull AtomicReference<T> reference, @Nullable T newValue) {
         T value = reference.get();
-        while (!reference.compareAndSet(value, newValue))
+        T oldValue = null;
+        while (!reference.compareAndSet(value, newValue)) {
+            oldValue = value;
             value = reference.get();
+        }
+        return oldValue == null ? value : oldValue;
     }
 }
