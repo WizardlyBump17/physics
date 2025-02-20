411e8a699c01e31a0e9c40f87c2ebd1a6818f349
WizardlyBump17 <davicraft130@gmail.com>
2025-02-20 15:31:00 -0300

    added the BaseObjectContainer class

---
+++ three/src/main/java/com/wizardlybump17/physics/three/container/BaseObjectContainer.java
@@ -0,0 +1,34 @@
+package com.wizardlybump17.physics.three.container;
+
+import com.wizardlybump17.physics.Tickable;
+import com.wizardlybump17.physics.three.object.BaseObject;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public abstract class BaseObjectContainer implements Tickable {
+
+    private final @NotNull UUID id;
+
+    public BaseObjectContainer(@NotNull UUID id) {
+        this.id = id;
+    }
+
+    public @NotNull UUID getId() {
+        return id;
+    }
+
+    public abstract void addObject(@NotNull BaseObject object);
+
+    public abstract @Nullable BaseObject getObject(int id);
+
+    public abstract boolean hasObject(int id);
+
+    public abstract void removeObject(int id);
+
+    public abstract @NotNull Collection<BaseObject> getLoadedObjects();
+
+    public abstract int getLoadedObjectsCount();
+}
