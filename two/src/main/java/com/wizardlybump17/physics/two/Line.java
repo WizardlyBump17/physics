6d546916ee689298c87071ac4f19f3af1cac40ab
WizardlyBump17 <davicraft130@gmail.com>
2025-04-10 11:00:39 -0300

    added the Line#overlaps(Line) method

--- two/src/main/java/com/wizardlybump17/physics/two/Line.java
+++ two/src/main/java/com/wizardlybump17/physics/two/Line.java
@@ -1,6 +1,7 @@
 package com.wizardlybump17.physics.two;
 
 import com.wizardlybump17.physics.two.position.Vector2D;
+import com.wizardlybump17.physics.two.util.CollisionsUtil;
 import org.jetbrains.annotations.NotNull;
 
 public record Line(@NotNull Vector2D start, @NotNull Vector2D end) {
@@ -11,5 +12,9 @@ public record Line(@NotNull Vector2D start, @NotNull Vector2D end) {
 
     public @NotNull Vector2D toVector() {
         return end.subtract(start);
+    }
+
+    public boolean overlaps(@NotNull Line other) {
+        return CollisionsUtil.overlapsLineToLine(this, other);
     }
 }
