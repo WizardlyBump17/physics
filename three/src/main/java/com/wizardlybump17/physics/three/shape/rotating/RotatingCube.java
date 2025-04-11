7000ce0feec670daf145b0cda7041852bdad8884
WizardlyBump17 <davicraft130@gmail.com>
2025-04-11 19:05:44 -0300

    sorting the points

--- three/src/main/java/com/wizardlybump17/physics/three/shape/rotating/RotatingCube.java
+++ three/src/main/java/com/wizardlybump17/physics/three/shape/rotating/RotatingCube.java
@@ -4,7 +4,9 @@ import com.wizardlybump17.physics.three.Vector3D;
 import com.wizardlybump17.physics.three.shape.Shape;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class RotatingCube extends Shape {
@@ -15,8 +17,12 @@ public class RotatingCube extends Shape {
     private final @NotNull Vector3D rotation;
 
     public RotatingCube(@NotNull Vector3D position, @NotNull List<Vector3D> points, @NotNull Vector3D rotation) {
+        this(position, points, rotation, false);
+    }
+
+    private RotatingCube(@NotNull Vector3D position, @NotNull List<Vector3D> points, @NotNull Vector3D rotation, boolean sorted) {
         this.position = position;
-        this.points = Collections.unmodifiableList(points);
+        this.points = Collections.unmodifiableList(sorted ? points : sortPoints(points));
         this.rotation = rotation;
         transformedPoints = points.stream()
                 .map(point -> position.add(point.rotateAround(rotation)))
@@ -74,5 +80,19 @@ public class RotatingCube extends Shape {
 
     public @NotNull RotatingCube withPoints(@NotNull List<Vector3D> points) {
         return new RotatingCube(position, points, rotation);
+    }
+
+    public static @NotNull List<Vector3D> sortPoints(@NotNull List<Vector3D> points) {
+        List<Vector3D> result = new ArrayList<>(points);
+
+        Vector3D sum = Vector3D.ZERO;
+        for (Vector3D point : points)
+            sum = sum.add(point);
+
+        Vector3D center = sum.divide(points.size());
+
+        result.sort(Comparator.comparingDouble(point -> point.subtract(center).lengthSquared()));
+
+        return result;
     }
 }
