483e646feb52627407d4a02a12d55de3c6727a22
WizardlyBump17 <davicraft130@gmail.com>
2025-06-30 20:35:42 -0300

    generic Ids

--- shared/src/main/java/com/wizardlybump17/physics/Id.java
+++ shared/src/main/java/com/wizardlybump17/physics/Id.java
@@ -2,10 +2,18 @@ package com.wizardlybump17.physics;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.UUID;
+
 public record Id(@NotNull String namespace, @NotNull String key) {
+
+    public static final @NotNull Id GENERIC = new Id("Generic", "Generic");
 
     @Override
     public @NotNull String toString() {
         return namespace + ":" + key;
+    }
+
+    public static @NotNull Id random() {
+        return new Id(UUID.randomUUID().toString(), UUID.randomUUID().toString());
     }
 }
